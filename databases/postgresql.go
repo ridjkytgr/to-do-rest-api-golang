8550d6aa413767cab1dc0d8860a1019947a8a729
ridjky.perkasa <ridjky.perkasa@shopee.com>
2024-06-29 16:59:07 +0700

    feat: create function to connect to postgresql database

---
+++ databases/postgresql.go
@@ -0,0 +1,20 @@
+package databases
+
+import (
+	"database/sql"
+	"os"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
